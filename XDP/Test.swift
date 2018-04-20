b25f1f1f03d77268e6c34a9374cbf8f083d881f9
wuxinting <wuxinting@baidu.com>
2018-04-20 11:57:14 +0800

    compile success

---
+++ XDP/Test.swift
@@ -0,0 +1,8 @@
+//
+//  Test.swift
+//  Pods
+//
+//  Created by Wu,Xinting on 2018/4/20.
+//
+
+import Foundation
---
+++ XDP/XDP.swift
@@ -0,0 +1,8 @@
+//
+//  XDP.swift
+//  Pods
+//
+//  Created by Wu,Xinting on 2018/4/20.
+//
+
+import Foundation
